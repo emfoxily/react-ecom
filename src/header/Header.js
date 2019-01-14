a03567bc4dc1b8397b486a44cceda18747fec52b
emily reno <emilyreno@emilys-MacBook-Pro.local>
2019-01-14 14:25:30 -0500

    working with react media npm package

--- src/header/Header.js
+++ src/header/Header.js
@@ -5,19 +5,21 @@ import search from '../assets/magnifier.png';
 class Header extends Component {
     render() {
         return(
-            <header className="site-header">
-                <h1 className="site-title">
-                    YOUR CUP OF <span className="sugar">
-                        sugar
-                    </span>
-                </h1>
-                <div className="search">
-                    <input type="text" placeholder="Search..." />
-                    <a href="/">
-                        <img className="search-image" src={search} alt="search"/>
-                    </a>
-                </div>
-            </header>
+            <div>
+                <header className="site-header">
+                    <h1 className="site-title">
+                        YOUR CUP OF <span className="sugar">
+                            sugar
+                        </span>
+                    </h1>
+                    <div className="search">
+                        <input type="text" placeholder="Search..." />
+                        <a href="/">
+                            <img className="search-image" src={search} alt="search"/>
+                        </a>
+                    </div>
+                </header>
+            </div>
         )
     }
 }
--- src/main-page/MainPage.js
+++ src/main-page/MainPage.js
@@ -1,11 +1,23 @@
+// dependencies
 import React, { Component } from 'react';
+import Media from 'react-media';
+
+// components
 import Header from '../header/Header.js';
 
 class MainPage extends Component {
     render() {
         return(
             <div>
-                <Header />
+                <Media query="(max-width: 480px)">
+                    {matches => 
+                        matches ? (
+                            <h1>the screen is 480px wide or less</h1>
+                        ) : (
+                            <Header />
+                        )
+                    }
+                </Media>
             </div>
         )
     }
